68ceba48130e152a45e6d8fef4466c0d14c7673a
Alexander Haas <mail@haas-alexander.com>
2023-01-16 20:28:00 +0100

    feat(project): :heavy_plus_sign: Netlify ssr adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
+import netlify from "@astrojs/netlify/functions";
+
+// https://astro.build/config
 export default defineConfig({
   integrations: [
     tailwind({
@@ -12,4 +15,6 @@ export default defineConfig({
       },
     }),
   ],
+  output: "server",
+  adapter: netlify(),
 });
