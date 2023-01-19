f0af3eed00e731c870a935fb5fd26e229137a209
Alexander Haas <mail@haas-alexander.com>
2023-01-19 10:06:52 +0100

    feat(ssr): Added example deug endpoints

--- src/pages/api/series.ts
+++ src/pages/api/series.ts
@@ -1,6 +1,7 @@
 import type { APIRoute } from "astro";
-import { getSerie } from "../../lib/crud";
+import { getSerie } from "../../lib/requests";
 
+// TODO: Replace with dynamic route
 export const get: APIRoute = async ({ params, request }) => {
   return {
     body: JSON.stringify({
