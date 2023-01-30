8b73cbcd398159c0b30f96b41436fdfc87316625
Alexander Haas <mail@haas-alexander.com>
2023-01-31 00:38:04 +0100

    dependecies + switched to vercel one last time

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,15 +4,17 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-import netlify from "@astrojs/netlify/functions";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    config: {
-      applyBaseStyles: false
-    }
-  })],
+  integrations: [
+    tailwind({
+      config: {
+        applyBaseStyles: false,
+      },
+    }),
+  ],
   output: "server",
-  adapter: netlify()
+  adapter: vercel(),
 });
