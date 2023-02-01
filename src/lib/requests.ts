1b9d981d930d4f1288426e4d44446cac22388c9b
Alexander Haas <mail@haas-alexander.com>
2023-02-01 13:24:31 +0100

    refactor(endpoints): removed minio functionality

--- src/lib/requests.ts
+++ src/lib/requests.ts
@@ -1,7 +1,5 @@
 import type { Document } from "mongodb";
 import { db } from "./mongo";
-import { redis } from "./redis";
-import { getFile } from "./minio";
 
 export async function getSerie(title: string): Promise<Document> {
   const cursor = db.collection("series").aggregate([
@@ -17,20 +15,4 @@ export async function getSerie(title: string): Promise<Document> {
   ]);
 
   return cursor.toArray();
-}
-
-export async function getFileCached(file: string) {
-  let buf = await redis.getBuffer(file);
-
-  if (!buf) {
-    console.log("File not cached");
-    buf = await getFile(file);
-    if (!buf) {
-      throw "File does not exists";
-    }
-    // fire and forget
-    redis.setex(file, 3600, buf);
-  }
-
-  return buf;
 }
